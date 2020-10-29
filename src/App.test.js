ade4c29b603080b78cce32bfc832cb06093795c7
kai-blt <blaitong@gmail.com>
2020-10-29 12:47:02 -0700

    added two tests to check first name and last name fields

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,42 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-test("renders App without crashing", () => {
-  render(<App />);
-});
+describe('Form Field Testing', () => {
+
+  test('First Name input exists', () => {
+    render(<App/>);
+    //Grab text input by placeholder
+    const firstName = screen.getAllByPlaceholderText(/Edd/i);
+    screen.debug(firstName)
+  });
+
+  test('First Name can be typed into', () => {
+    render(<App/>);
+    const firstName = screen.getByPlaceholderText(/Edd/i);
+    //It should be empty to start
+    expect(firstName).not.toHaveValue()
+    //Type into field
+    fireEvent.change(firstName, {target: {name: 'firstName', value: 'Brendan'}})
+    //Print out debug
+    screen.debug(firstName);
+    //Check that correct value was input
+    expect(firstName).toHaveValue('Brendan');
+  });
+
+  test('Last Name can be typed into', () => {
+    render(<App/>);
+    const firstName = screen.getByPlaceholderText(/Burke/i);
+    //It should be empty to start
+    expect(firstName).not.toHaveValue()
+    //Type into field
+    fireEvent.change(firstName, {target: {name: 'firstName', value: 'LT'}})
+    //Print out debug
+    screen.debug(firstName);
+    //Check that correct value was input
+    expect(firstName).toHaveValue('LT');
+  });
+
+
+
+})
