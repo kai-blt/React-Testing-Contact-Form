83352a8c0b987e4fa1f386313afb72eec6a6d08e
kai-blt <blaitong@gmail.com>
2020-10-29 18:18:39 -0700

    increased firstname maxlength and added check for form submit

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import App from "./App";
 import ContactForm from './components/ContactForm'
 import { act } from "react-dom/test-utils";
@@ -55,24 +55,25 @@ describe('Form Field Testing', () => {
   });
 
   test('Filling out form completely and submitting results in json printout', async () => {
-    await act(async () => {
-      render(<App/>);
-      const firstName = screen.getByLabelText(/First Name/i);
-      const lastName = screen.getByLabelText(/Last Name/i);
-      const email = screen.getByLabelText(/Email/i);
-      const message = screen.getByLabelText(/Message/i);
+    render(<App/>);
+    const firstName = screen.getByLabelText(/First Name/i);
+    const lastName = screen.getByLabelText(/Last Name/i);
+    const email = screen.getByLabelText(/Email/i);
+    const message = screen.getByLabelText(/Message/i);
 
-      fireEvent.change(firstName, {target: {name: 'firstName', value: 'Brendan'}})
-      fireEvent.change(lastName, {target: {name: 'lastName', value: 'LT'}})
-      fireEvent.change(email, {target: {name: 'email', value: 'email@email.com'}})
-      fireEvent.change(message, {target: {name: 'message', value: 'this is a message'}})
 
-      const button = screen.getByTestId(/submit/i);  
-      fireEvent.click(button);        
-    });
+    fireEvent.change(firstName, {target: {name: 'firstName', value: 'Brendan'}})
+    fireEvent.change(lastName, {target: {name: 'lastName', value: 'LT'}})
+    fireEvent.change(email, {target: {name: 'email', value: 'email@email.com'}})
+    fireEvent.change(message, {target: {name: 'message', value: 'this is a message'}})
+
+
+    const button = screen.getByTestId(/submit/i);  
+    fireEvent.click(button);      
     
-    const errors = screen.getByText(/{}/i);
-    expect(errors).toHaveValue(/{}/i);
+    await waitFor(() => {
+      expect(screen.getByText(/Bre/i)).toBeInTheDocument();
+    });
   })
 
 })
